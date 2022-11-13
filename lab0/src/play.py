f21fde7ab5bdecb024fecf5fbaf8d25be5f0ef9c
Franco Ruggeri <francoruggeri95@gmail.com>
2022-11-13 17:01:51 +0100

    Add separate mazes with and without delay

--- lab0/src/play.py
+++ lab0/src/play.py
@@ -2,7 +2,8 @@ from pathlib import Path
 from maze import Maze
 from dynamic_programming import DynamicProgrammingAgent
 
-map_filepath = Path(__file__).parent.parent / "data" / "maze.txt"
+# map_filepath = Path(__file__).parent.parent / "data" / "maze.txt"
+map_filepath = Path(__file__).parent.parent / "data" / "maze_delay.txt"
 
 env = Maze(map_filepath=map_filepath, horizon=13)
 env.seed(1)
