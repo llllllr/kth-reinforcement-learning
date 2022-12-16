5967fe0fad1d4a9537d22aac4f3da05e3aea8c7a
Franco Ruggeri <franco.ruggeri@ericsson.com>
2022-12-16 22:43:14 +0100

    Update RandomAgent

--- el2805/agents/random.py
+++ el2805/agents/random.py
@@ -1,14 +1,10 @@
-import gym
 import numpy as np
 from el2805.agents.agent import Agent
 
 
 class RandomAgent(Agent):
-    def __init__(self, environment: gym.Env):
-        super(RandomAgent, self).__init__(environment=environment)
-        assert isinstance(environment.action_space, gym.spaces.Discrete)
-        self._n_actions = environment.action_space.n
-
     def compute_action(self, *, state: np.ndarray, **kwargs) -> int:
         _ = kwargs
-        return np.random.randint(0, self._n_actions)
+        _ = state
+        action = self.environment.action_space.sample()
+        return action
