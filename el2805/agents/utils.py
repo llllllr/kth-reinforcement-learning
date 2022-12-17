d9ce0b4e7cb80ce747768c07310fda59a86fa5fc
Franco Ruggeri <franco.ruggeri@ericsson.com>
2022-12-17 11:54:00 +0100

    Update running average
    Now, it computes also the average of the first episodes.

--- el2805/agents/utils.py
+++ el2805/agents/utils.py
@@ -2,9 +2,8 @@ import numpy as np
 
 
 def running_average(data, window_length: int = 50):
-    if len(data) >= window_length:
-        averages = np.copy(data)
-        averages[window_length - 1:] = np.convolve(data, np.ones((window_length,)) / window_length, mode='valid')
-    else:
-        averages = np.zeros_like(data)
+    overlap_length = np.concatenate((np.arange(1, window_length), window_length * np.ones(len(data))))
+    window = np.ones(window_length)
+    averages = (np.convolve(data, window) / overlap_length)[:-(window_length-1)]
+    assert len(averages) == len(data)
     return averages
